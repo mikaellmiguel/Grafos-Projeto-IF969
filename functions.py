621fe6889e7e3b1b63038c2282da30614014ca93
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2024-02-17 19:34:39 -0300

    feat: construção da mst gerada pelo algoritmo de prim

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from classes import *
 
 # Implementação do algoritmo de prim
-def prim(grafo: Grafo, vertice_inicial:int):
+def prim(grafo: Grafo, vertice_inicial:int, tabela):
 
     matriz = grafo.get_matriz() # Obtendo a matriz de adjacência do referente grafo
 
@@ -16,9 +16,14 @@ def prim(grafo: Grafo, vertice_inicial:int):
     fila.heap[vertice_inicial] = [vertice_inicial, 0] # Inserido o vertice inicial com custo 0 na fila
     fila.heapify() # Ordenando a Fila
 
+    mst = []  #Armazenar MST
     while fila:
         vertice, valor = fila.remocao() # Extraindo o mínimo através do Heap
         vertices_fila[vertice] = False # Marcando como False informando que ele não está mais na fila
+        
+        # Adicionando as arestas na mst, conforme cada vertice vai saindo da fila
+        if parent[vertice] is not None:
+            mst.append((tabela["local"][parent[vertice]], tabela["local"][vertice], round(valor * 1000, 2)))
 
         for i in range(grafo.get_tamanho()):
             if matriz[vertice][i] != 0:  # Se tiver aresta
@@ -26,3 +31,6 @@ def prim(grafo: Grafo, vertice_inicial:int):
                     custo[i] = matriz[vertice][i] # Substituindo o custo anterior pelo peso atual
                     parent[i] = vertice  # Substituindo o predecessor do vertice
                     fila.alterar_chave(i, matriz[vertice][i])  # Alternado a chave na fila de prioridades
+
+    # Retornando a MST e o custo total da MST
+    return mst, sum(custo)
